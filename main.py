27068cf3f73eb1aac38dc398484f94b7203b8486
Erik De Luca <61795785+erikdeluca@users.noreply.github.com>
2024-01-03 13:57:08 +0100

    Add check_image_size
    Now, the user can choose the different image sizes for both models (dall-e-2 and dall-e-3). The system will check if the image size is right for the selected model.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from cat.mad_hatter.decorators import tool, plugin
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
+from cat.log import log
 from enum import Enum
 import requests
-import json
+import json    
 
 class ImageSize(Enum):
     quad: str = '1024x1024'
     width: str = '1792x1024'
     height: str = '1024x1792'
+    medium: str = '512x512'
+    low: str = '256x256'
 
 class Quality(Enum):
     hd: str = 'hd'
@@ -22,16 +25,31 @@ class Model(Enum):
     dalle2: str = 'dall-e-2'
 
 class Settings(BaseModel):
-    api_key: str = Field(title="API Key", description="The API key for OpenAI's image generation API.", default="")
-    image_size: ImageSize = Field(title="Image size", description="The size for the image to generate", default=ImageSize.quad)
-    quality: Quality = Field(title="Image quality", description="The quality for the image to generate", default=Quality.hd)
-    style: Style = Field(title="Image style", description="The style for the image to generate", default=Style.natural)
-    model: Model = Field(title="Model", description="The model to use for the image to generate", default=Model.dalle3)
+    api_key: str = Field(title="API Key", description="The API key for OpenAI's image generation API.", default=""),
+    image_size: ImageSize = Field(title="Image size", description="The size for the image to generate. For dall-e-3 you can choose between '1024x1024', '1792x1024' and '1024x1792' while for dall-e-2 you can choose between '1024x1024', '512x512' and '256x256'", default=ImageSize.quad),
+    quality: Quality = Field(title="Image quality", description="The quality for the image to generate. It will be used only with dall-e-3", default=Quality.hd),
+    style: Style = Field(title="Image style", description="The style for the image to generate. It will be used only with dall-e-3", default=Style.natural),
+    model: Model = Field(title="Model", description="The model to use for the image to generate", default=Model.dalle3)    
     
+    @model_validator(mode='after')
+    def check_image_size_validator(self) -> 'Settings':
+        model = self.model
+        threshold = self.image_size
+        if  (threshold == ImageSize.width and model == Model.dalle2) or (threshold == ImageSize.height and model == Model.dalle2):
+            raise ValueError("Image size not acepted for this model, please select a '1024x1024' or less")
+        if  (threshold == ImageSize.medium and model == Model.dalle3) or (threshold == ImageSize.low and model == Model.dalle3):
+            raise ValueError("Image size not acepted for this model, please select a '1024x1024' or more")
+        return self
 
+
+    
 @plugin
 def settings_schema():   
-    return Settings.schema()
+   return Settings.schema()
+    
+# @plugin
+# def settings_model():
+    # return Settings
 
 @tool(return_direct=True)
 def generate_image(tool_input, cat):
@@ -63,4 +81,4 @@ def generate_image(tool_input, cat):
 
     size = settings['image_size'].split('x')
 
-    return f"<img src='{image}' style='width: {int(size[0]) / 2}px; height: {int(size[1]) / 2}px;' alt='Generated image' />"
+    return f"<img src='{image}' style='width: {int(size[0]) / 2}px; height: {int(size[1]) / 2}px;' alt='Generated image' />" # it could be nice to create a download button for the original quality and not the half
